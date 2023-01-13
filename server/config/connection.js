c3c7f7e18c6da36d287ffa400a9ede0bde31a4ed
JesusGonzalez05 <Jesusgonzalez051698@gmail.com>
2023-01-13 00:55:59 -0500

    debug post on sign up.

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,8 +1,11 @@
 const mongoose = require('mongoose');
 
-mongoose.connect(process.env.MONGO_URI || 'mongodb://localhost:27017/only-paws', {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-});
+mongoose.connect(
+  process.env.MONGODB_URI || 'mongodb://localhost:27017/test',
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  },
+);
 
 module.exports = mongoose.connection;
